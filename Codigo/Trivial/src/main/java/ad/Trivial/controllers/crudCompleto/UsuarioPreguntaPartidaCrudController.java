3556e10652bca07410208887680b9c0b19f07c5e
Iván <ivanbapi@gmail.com>
2025-02-14 11:16:27 +0100

    s

--- Codigo/Trivial/src/main/java/ad/Trivial/controllers/crudCompleto/UsuarioPreguntaPartidaCrudController.java
+++ Codigo/Trivial/src/main/java/ad/Trivial/controllers/crudCompleto/UsuarioPreguntaPartidaCrudController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controlador CRUD para la entidad UsuarioPreguntaPartida.
+ * Proporciona endpoints para realizar operaciones CRUD en las relaciones entre usuarios, preguntas y partidas.
+ */
 @RestController
 @RequestMapping("/crud/UsPrPa")
 public class UsuarioPreguntaPartidaCrudController {
@@ -14,21 +18,43 @@ public class UsuarioPreguntaPartidaCrudController {
     @Autowired
     UsuarioPreguntaPartidaService usuarioPreguntaPartidaService;
 
+    /**
+     * Obtiene todas las relaciones UsuarioPreguntaPartida.
+     *
+     * @return una lista de todas las relaciones UsuarioPreguntaPartida
+     */
     @GetMapping
     public List<UsuarioPreguntaPartida> obtenerTodas(){
         return usuarioPreguntaPartidaService.obtenerTodas();
     }
 
+    /**
+     * Guarda una nueva relación UsuarioPreguntaPartida.
+     *
+     * @param usuarioPreguntaPartida la relación UsuarioPreguntaPartida a guardar
+     * @return la relación UsuarioPreguntaPartida guardada
+     */
     @PostMapping
     public UsuarioPreguntaPartida guardar(@RequestBody UsuarioPreguntaPartida usuarioPreguntaPartida){
         return usuarioPreguntaPartidaService.guardar(usuarioPreguntaPartida);
     }
 
+    /**
+     * Actualiza una relación UsuarioPreguntaPartida existente.
+     *
+     * @param usuarioPreguntaPartida la relación UsuarioPreguntaPartida a actualizar
+     * @return la relación UsuarioPreguntaPartida actualizada
+     */
     @PutMapping
     public UsuarioPreguntaPartida actualizar(@RequestBody UsuarioPreguntaPartida usuarioPreguntaPartida){
         return usuarioPreguntaPartidaService.actualizar(usuarioPreguntaPartida);
     }
 
+    /**
+     * Elimina una relación UsuarioPreguntaPartida por su ID.
+     *
+     * @param id el ID de la relación UsuarioPreguntaPartida a eliminar
+     */
     @DeleteMapping("/{id}")
     public void borrar(@PathVariable Long id){
         usuarioPreguntaPartidaService.eliminar(id);
--- Codigo/Trivial/src/main/java/ad/Trivial/services/UsuarioService.java
+++ Codigo/Trivial/src/main/java/ad/Trivial/services/UsuarioService.java
@@ -1,8 +1,10 @@
 package ad.Trivial.services;
 
 import ad.Trivial.models.Usuario;
+import ad.Trivial.models.modelosDTO.UsuariODTO;
 import ad.Trivial.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +14,8 @@ public class UsuarioService {
 
     @Autowired
     UsuarioRepository usuarioRepository;
+    @Autowired
+    ConversionModelos conversionModelos;
 
     public List<Usuario> obtenerTodas(){
         return usuarioRepository.findAll();
@@ -30,6 +34,27 @@ public class UsuarioService {
     }
     public void eliminar(Long id){
         usuarioRepository.deleteById(id);
+    }
+
+    //Parte DTOs
+    public ResponseEntity<?> obtenerPorId(Long id){
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        if (usuario != null){
+            UsuariODTO usuariODTO = conversionModelos.transformarUsuarioADTO(usuario);
+            return ResponseEntity.ok(usuariODTO);
+        }else{
+            return ResponseEntity.status(404).body("No se ha encontrado el usuario");
+        }
+    }
+
+    public ResponseEntity<?> guardarFront(Usuario usuario){
+        Usuario usuarioGuardado = usuarioRepository.save(usuario);
+        if (usuarioGuardado != null){
+            UsuariODTO usuariODTO = conversionModelos.transformarUsuarioADTO(usuarioGuardado);
+            return ResponseEntity.ok(usuariODTO);
+        }else{
+            return ResponseEntity.status(500).body("Error al guardar el usuario");
+        }
     }
 
 }
